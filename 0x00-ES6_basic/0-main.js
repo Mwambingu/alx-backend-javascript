dd12e73415beffcbc30c2e9b25f48076ec9976a9
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-12-19 22:03:08 +0300

    Advanced Task II

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -85,3 +85,19 @@ const reportWithIterator = createIteratorObject(report);
 for (const item of reportWithIterator) {
     console.log(item);
 }
+//Advanced Task II
+import createEmployeesObject from "./11-createEmployeesObject.js";
+import createReportObject from './12-createReportObject.js';
+import createIteratorObject from './100-createIteratorObject.js';
+import iterateThroughObject from './101-iterateThroughObject.js';
+
+
+const employees = {
+    ...createEmployeesObject('engineering', ['Bob', 'Jane']),
+    ...createEmployeesObject('marketing', ['Sylvie'])
+};
+
+const report = createReportObject(employees);
+const reportWithIterator = createIteratorObject(report);
+
+console.log(iterateThroughObject(reportWithIterator));
