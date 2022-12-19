51804f42ffc5decfa1278a6749732c24aa8f7de8
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-12-19 21:03:57 +0300

    Task 2

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,8 +1,15 @@
+//Task 0
 import { taskFirst, taskNext } from './0-constants.js';
 
 console.log(`${taskFirst()} ${taskNext()}`);
-
+//Task 1
 import taskBlock from './1-block-scoped.js';
 
 console.log(taskBlock(true));
 console.log(taskBlock(false));
+//Task 2
+import getNeighborhoodsList from './2-arrow.js';
+
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
