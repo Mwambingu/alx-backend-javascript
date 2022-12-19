cba0a11d868d3073cfec5914612ebceef04c2ba0
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-12-19 21:50:12 +0300

    Task 10

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -47,3 +47,7 @@ const fullBudget = getFullBudgetObject(20, 50, 10);
 
 console.log(fullBudget.getIncomeInDollars(fullBudget.income));
 console.log(fullBudget.getIncomeInEuros(fullBudget.income));
+//Task 10
+import appendToEachArrayValue from './10-loops.js';
+
+console.log(appendToEachArrayValue(['appended', 'fixed', 'displayed'], 'correctly-'));
