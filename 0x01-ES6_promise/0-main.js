7154c4c9899530d6147a886decbd4ad2dc55bee2
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-12-21 20:10:25 +0300

    Advanced Task I

--- 0x01-ES6_promise/0-main.js
+++ 0x01-ES6_promise/0-main.js
@@ -64,3 +64,13 @@ import divideFunction from './8-try';
 
 console.log(guardrail(() => { return divideFunction(10, 2)}));
 console.log(guardrail(() => { return divideFunction(10, 0)}));
+
+//Advanced I
+import asyncUploadUser from "./100-await";
+
+const test = async () => {
+    const value = await asyncUploadUser();
+    console.log(value);
+};
+
+test();
