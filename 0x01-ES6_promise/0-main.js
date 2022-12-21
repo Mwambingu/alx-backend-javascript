63da5530d6c0819542d20240ca062deaafd9d1b0
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-12-21 19:16:00 +0300

    Task 2

--- 0x01-ES6_promise/0-main.js
+++ 0x01-ES6_promise/0-main.js
@@ -4,7 +4,12 @@ import getResponseFromAPI from "./0-promise.js";
 const response = getResponseFromAPI();
 console.log(response instanceof Promise);
 //Task 1
-import getFullResponseFromAPI from './1-promise';
+// import getFullResponseFromAPI from './1-promise';
 
-console.log(getFullResponseFromAPI(true));
-console.log(getFullResponseFromAPI(false));
+// console.log(getFullResponseFromAPI(true));
+// console.log(getFullResponseFromAPI(false));
+//Task 2
+import handleResponseFromAPI from "./2-then";
+
+const promise = Promise.resolve();
+handleResponseFromAPI(promise);
