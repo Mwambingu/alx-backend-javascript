886eb977e7c9f687cc81e0af7408d0ad32b403f9
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-12-21 19:20:18 +0300

    Hot Fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,7 @@
+/* eslint-disable */
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    console.log('Got a response from the API');
-  }, (err) => err);
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
