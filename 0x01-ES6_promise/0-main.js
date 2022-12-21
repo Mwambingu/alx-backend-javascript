2dfe9314b266965db0875bc11f9b941f839cbdc9
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-12-21 19:50:46 +0300

    Task 7

--- 0x01-ES6_promise/0-main.js
+++ 0x01-ES6_promise/0-main.js
@@ -9,23 +9,47 @@ console.log(response instanceof Promise);
 // console.log(getFullResponseFromAPI(true));
 // console.log(getFullResponseFromAPI(false));
 //Task 2
-import handleResponseFromAPI from "./2-then";
+// import handleResponseFromAPI from "./2-then";
 
-const promise = Promise.resolve();
-handleResponseFromAPI(promise);
-//Task 3
-import handleProfileSignup from "./3-all";
+// const promise = Promise.resolve();
+// handleResponseFromAPI(promise);
+// //Task 3
+// import handleProfileSignup from "./3-all";
 
-handleProfileSignup();
-//Task 4
-import signUpUser from "./4-user-promise";
+// handleProfileSignup();
+// //Task 4
+// import signUpUser from "./4-user-promise";
 
-console.log(signUpUser("Bob", "Dylan"));
-//Task 5
-import uploadPhoto from './5-photo-reject';
+// console.log(signUpUser("Bob", "Dylan"));
+// //Task 5
+// import uploadPhoto from './5-photo-reject';
 
-console.log(uploadPhoto('guillaume.jpg'));
-//Task 6
-import handleProfileSignup from './6-final-user';
+// console.log(uploadPhoto('guillaume.jpg'));
+// //Task 6
+// import handleProfileSignup from './6-final-user';
 
-console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
+// console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
+//Task 7
+import loadBalancer from "./7-load_balancer";
+
+const ukSuccess = 'Downloading from UK is faster';
+const frSuccess = 'Downloading from FR is faster';
+
+const promiseUK = new Promise(function(resolve, reject) {
+    setTimeout(resolve, 100, ukSuccess);
+});
+
+const promiseUKSlow = new Promise(function(resolve, reject) {
+    setTimeout(resolve, 400, ukSuccess);
+});
+
+const promiseFR = new Promise(function(resolve, reject) {
+    setTimeout(resolve, 200, frSuccess);
+});
+
+const test = async () => {
+    console.log(await loadBalancer(promiseUK, promiseFR));
+    console.log(await loadBalancer(promiseUKSlow, promiseFR));
+}
+
+test();
