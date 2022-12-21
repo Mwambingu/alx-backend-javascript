d0f7b6c4dfa84e6cde3dc71c787f279330b2fd09
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-12-21 20:03:21 +0300

    Task 9

--- 0x01-ES6_promise/0-main.js
+++ 0x01-ES6_promise/0-main.js
@@ -30,31 +30,37 @@ console.log(response instanceof Promise);
 
 // console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
 //Task 7
-import loadBalancer from "./7-load_balancer";
+// import loadBalancer from "./7-load_balancer";
 
-const ukSuccess = 'Downloading from UK is faster';
-const frSuccess = 'Downloading from FR is faster';
+// const ukSuccess = 'Downloading from UK is faster';
+// const frSuccess = 'Downloading from FR is faster';
 
-const promiseUK = new Promise(function(resolve, reject) {
-    setTimeout(resolve, 100, ukSuccess);
-});
+// const promiseUK = new Promise(function(resolve, reject) {
+//     setTimeout(resolve, 100, ukSuccess);
+// });
 
-const promiseUKSlow = new Promise(function(resolve, reject) {
-    setTimeout(resolve, 400, ukSuccess);
-});
+// const promiseUKSlow = new Promise(function(resolve, reject) {
+//     setTimeout(resolve, 400, ukSuccess);
+// });
 
-const promiseFR = new Promise(function(resolve, reject) {
-    setTimeout(resolve, 200, frSuccess);
-});
+// const promiseFR = new Promise(function(resolve, reject) {
+//     setTimeout(resolve, 200, frSuccess);
+// });
 
-const test = async () => {
-    console.log(await loadBalancer(promiseUK, promiseFR));
-    console.log(await loadBalancer(promiseUKSlow, promiseFR));
-}
+// const test = async () => {
+//     console.log(await loadBalancer(promiseUK, promiseFR));
+//     console.log(await loadBalancer(promiseUKSlow, promiseFR));
+// }
 
-test();
-//Task 8
+// test();
+// //Task 8
+// import divideFunction from './8-try';
+
+// console.log(divideFunction(10, 2));
+// console.log(divideFunction(10, 0));
+//Task 9
+import guardrail from './9-try';
 import divideFunction from './8-try';
 
-console.log(divideFunction(10, 2));
-console.log(divideFunction(10, 0));
+console.log(guardrail(() => { return divideFunction(10, 2)}));
+console.log(guardrail(() => { return divideFunction(10, 0)}));
