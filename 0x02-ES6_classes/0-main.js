f4d43e0d29319db529e9bcd67efef646e43ddcd0
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-12-22 16:45:32 +0300

    Task 1

--- 0x02-ES6_classes/0-main.js
+++ 0x02-ES6_classes/0-main.js
@@ -3,3 +3,8 @@ import ClassRoom from "./0-classroom.js";
 
 const room = new ClassRoom(10);
 console.log(room._maxStudentsSize)
+
+// Task 1
+import initializeRooms from './1-make_classrooms.js';
+
+console.log(initializeRooms());
