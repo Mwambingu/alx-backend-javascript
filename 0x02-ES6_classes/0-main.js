2d621dd7b5c69b6f6b8dffb09d2d5ea0859bbd0d
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-12-22 18:28:27 +0300

    Advanced Task I

--- 0x02-ES6_classes/0-main.js
+++ 0x02-ES6_classes/0-main.js
@@ -106,3 +106,12 @@ console.log(tc2);
 console.log(tc2 instanceof TestCar);
 
 console.log(tc1 == tc2);
+
+//Advanced Task I
+import EVCar from './100-evcar.js';
+
+const ec1 = new EVCar("Tesla", "Turbo", "Red", "250");
+console.log(ec1);
+
+const ec2 = ec1.cloneCar();
+console.log(ec2);
