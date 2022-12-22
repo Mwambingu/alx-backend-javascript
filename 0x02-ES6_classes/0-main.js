5f9b25cec09829e3ee4578b5ac042f956cc6aa66
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-12-22 17:09:35 +0300

    Task 3

--- 0x02-ES6_classes/0-main.js
+++ 0x02-ES6_classes/0-main.js
@@ -10,23 +10,29 @@ import initializeRooms from './1-make_classrooms.js';
 console.log(initializeRooms());
 
 // Task 2
-import HolbertonCourse from "./2-hbtn_course.js";
+// import HolbertonCourse from "./2-hbtn_course.js";
 
-const c1 = new HolbertonCourse("ES6", 1, ["Bob", "Jane"])
-console.log(c1.name);
-c1.name = "Python 101";
-console.log(c1);
+// const c1 = new HolbertonCourse("ES6", 1, ["Bob", "Jane"])
+// console.log(c1.name);
+// c1.name = "Python 101";
+// console.log(c1);
 
-try {
-    c1.name = 12;
-} 
-catch(err) {
-    console.log(err);
-}
+// try {
+//     c1.name = 12;
+// } 
+// catch(err) {
+//     console.log(err);
+// }
 
-try {
-    const c2 = new HolbertonCourse("ES6", "1", ["Bob", "Jane"]);
-}
-catch(err) {
-    console.log(err);
-}
+// try {
+//     const c2 = new HolbertonCourse("ES6", "1", ["Bob", "Jane"]);
+// }
+// catch(err) {
+//     console.log(err);
+// }
+
+//Task 3
+import Currency from "./3-currency.js";
+
+const dollar = new Currency('$', 'Dollars');
+console.log(dollar.displayFullCurrency());
