7b4eeba186cda3ca05196f58f1532db1d9629f8e
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-01-03 17:46:53 +0300

    Hot Fix

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,14 +1,4 @@
-/**
- * Retrieves ids from a list of students.
- * @param {{
-*   id: Number,
-*   firstName: String,
-*   location: String
-* }[]} students - The list of students.
-* @author Tonny Mwambingu
-* @returns
-*/
-
+/* eslint-disable */
 export default function getListStudentIds(students) {
   if (students instanceof Array) {
     return students.map((student) => student.id);
