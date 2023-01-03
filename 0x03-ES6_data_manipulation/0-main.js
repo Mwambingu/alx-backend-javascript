1c75957be8bab0eb69242f4d378def6bf9617ae8
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-01-03 17:55:04 +0300

    Task 2

--- 0x03-ES6_data_manipulation/0-main.js
+++ 0x03-ES6_data_manipulation/0-main.js
@@ -4,8 +4,15 @@
 
 // console.log(getListStudents());
 // Task 1
-import getListStudentIds from "./1-get_list_student_ids.js";
-import getListStudents from "./0-get_list_students.js";
+// import getListStudentIds from "./1-get_list_student_ids.js";
+// import getListStudents from "./0-get_list_students.js";
 
-console.log(getListStudentIds("hello"));
-console.log(getListStudentIds(getListStudents()));
+// console.log(getListStudentIds("hello"));
+// console.log(getListStudentIds(getListStudents()));
+// Task 2
+import getListStudents from "./0-get_list_students.js";
+import getStudentsByLocation from "./2-get_students_by_loc.js";
+
+const students = getListStudents();
+
+console.log(getStudentsByLocation(students, 'San Francisco'));
