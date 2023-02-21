460fe785a6d327b36103b4add3dce23327ed16a1
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-02-21 19:36:12 +0300

    Hot Fix

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 /**
  * Counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Tonny Mwambingu <https://github.com/Mwambingu>
  */
 const countStudents = (dataPath) =>
     new Promise((resolve, reject) => {
